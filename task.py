d1e6a1b4d1fdddff2dd29305597b3e8ad659aae4
kostyantin <kostyantin1408@gmail.com>
2022-11-07 17:48:58 +0200

    our_task

---
+++ task.py
@@ -0,0 +1,16 @@
+def climbing_stairs(number: int) -> int | None:
+    """
+    (int) -> (int | NoneType)
+    >>> climbing_stairs(2)
+    2
+    >>> climbing_stairs(1)
+    1
+    >>> climbing_stairs(12)
+    233
+    >>> climbing_stairs(-1)
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
